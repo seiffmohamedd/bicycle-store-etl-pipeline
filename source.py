6de95414871bd846d74121ae7241b3709870bb57
seiffmohamedd <seif500mohamed@gmail.com>
2025-04-16 11:17:07 +0200

    Layers Clarified

--- source.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd 
-import numpy as np
-from sqlalchemy import create_engine , text , table , DateTime
-
-
-df=pd.read_csv('E:\\ITI 9 Months\\Python\\Days\\Use Case\\source\\customers.csv')
-df.head(10)
